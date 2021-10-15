ee15c312af8e3218aaf9c9ff2c4d5abc03eccd6d
AraxTheCoder <krueger.jonathan@gmx.de>
2021-10-15 08:48:06 +0200

    Added default Colors

--- lib/Utils/ColorChooser.dart
+++ lib/Utils/ColorChooser.dart
@@ -2,6 +2,8 @@ import 'dart:collection';
 import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:pgu/Storage/StorageKeys.dart';
 import 'package:pgu/Storage/StorageManager.dart';
 import 'package:pgu/Values/Design/PGUColors.dart';
@@ -38,9 +40,16 @@ class ColorChooser{
   static void loadColors(){
     String jsonString = StorageManager.getString(StorageKeys.farben);
 
-    if(jsonString.isEmpty)
-      colors = Map<String, int>();
-    else
+    if(jsonString.isEmpty) {
+      colors = {
+        "CH" : Colors.purple.value,
+        "D" : Colors.green.value,
+        "E" : Colors.pink.value,
+        "IF" : Colors.blueGrey.value,
+        "M" : Colors.indigo.value,
+        "PH" : Colors.lightBlue.value,
+      };
+    }else
       colors = Map<String, int>.from(jsonDecode(jsonString));
   }
 }
